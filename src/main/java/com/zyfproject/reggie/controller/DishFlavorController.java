a1ce9173ba6bc396034428ecb7ba8a4496332b20
nuaazyf <nuaazyf@163.com>
2023-10-15 16:14:53 +0800

    菜品分类数据查询

--- src/main/java/com/zyfproject/reggie/controller/DishFlavorController.java
+++ src/main/java/com/zyfproject/reggie/controller/DishFlavorController.java
@@ -1,15 +1,15 @@
 package com.zyfproject.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zyfproject.reggie.common.R;
 import com.zyfproject.reggie.dto.DishDto;
+import com.zyfproject.reggie.entity.Dish;
 import com.zyfproject.reggie.service.DishFlavorService;
 import com.zyfproject.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dish")
@@ -30,5 +30,27 @@ public class DishFlavorController {
     @PostMapping
     public R<String> save(@RequestBody DishDto dishDto){
         return null;
+    }
+
+    /**
+     * 菜品信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+
+        // 条件构造器
+        LambdaQueryWrapper<Dish> queryWarpper = new LambdaQueryWrapper<>();
+        queryWarpper.like(name != null, Dish::getName, name);
+        // 排序条件，根据更新时间降序排序
+        queryWarpper.orderByDesc(Dish::getUpdateTime);
+
+        dishService.page(pageInfo, queryWarpper);
+
+        return R.success(pageInfo);
     }
 }
