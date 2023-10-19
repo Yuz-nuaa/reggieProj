c04fd15cc26500b351019530174b50602843c265
zyf <nuaazyf@163.com>
2023-10-19 22:47:05 +0800

    update修改口味/保存口味

--- src/main/java/com/zyfproject/reggie/controller/DishFlavorController.java
+++ src/main/java/com/zyfproject/reggie/controller/DishFlavorController.java
@@ -2,14 +2,21 @@ package com.zyfproject.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.zyfproject.reggie.common.R;
 import com.zyfproject.reggie.dto.DishDto;
+import com.zyfproject.reggie.entity.Category;
 import com.zyfproject.reggie.entity.Dish;
+import com.zyfproject.reggie.service.CategoryService;
 import com.zyfproject.reggie.service.DishFlavorService;
 import com.zyfproject.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/dish")
@@ -20,7 +27,10 @@ public class DishFlavorController {
     private DishService dishService;
 
     @Autowired
-    private DishFlavorService dishFlavorService;
+    private DishFlavorService dishFlaDivorService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     /**
      * 新增菜品
@@ -42,7 +52,7 @@ public class DishFlavorController {
     @GetMapping("/page")
     public R<Page> page(int page, int pageSize, String name) {
         Page<Dish> pageInfo = new Page<>(page, pageSize);
-
+        Page<DishDto> dishDtoPage = new Page<>();
         // 条件构造器
         LambdaQueryWrapper<Dish> queryWarpper = new LambdaQueryWrapper<>();
         queryWarpper.like(name != null, Dish::getName, name);
@@ -51,6 +61,53 @@ public class DishFlavorController {
 
         dishService.page(pageInfo, queryWarpper);
 
+        // 对象拷贝
+        BeanUtils.copyProperties(pageInfo, dishDtoPage, "records");
+
+        List<Dish> records = pageInfo.getRecords();
+        List<DishDto> list = records.stream().map((item)->{
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item,dishDto);
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+            if(category != null){
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+
         return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id查询菜品信息和对应的口味信息
+     *
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<DishDto> get(@PathVariable Long id){
+
+        DishDto dishDto = dishService.getByIdWithFlavor(id);
+
+        return R.success(dishDto);
+    }
+
+    /**
+     * 修改菜品
+     *
+     * @param dishDto
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody DishDto dishDto){
+        log.info(dishDto.toString());
+
+        dishService.updateWithFlavor(dishDto);
+
+        return R.success("新增菜品成功");
     }
 }
--- src/main/java/com/zyfproject/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/zyfproject/reggie/service/impl/DishServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zyfproject.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zyfproject.reggie.dto.DishDto;
 import com.zyfproject.reggie.entity.Dish;
@@ -8,6 +9,7 @@ import com.zyfproject.reggie.mapper.DishMapper;
 import com.zyfproject.reggie.service.DishFlavorService;
 import com.zyfproject.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,6 +41,39 @@ public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements Di
             return item;
         }).collect(Collectors.toList());
         // 保存菜品口味到菜品口味表
+        dishFlavorService.saveBatch(flavors);
+    }
+
+    @Override
+    public DishDto getByIdWithFlavor(Long id) {
+        Dish dish = this.getById(id);
+
+        DishDto dishDto = new DishDto();
+        BeanUtils.copyProperties(dish, dishDto);
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId, dish.getId());
+        List<DishFlavor> flavors = dishFlavorService.list(queryWrapper);
+        dishDto.setFlavors(flavors);
+
+        return dishDto;
+    }
+
+    @Override
+    public void updateWithFlavor(DishDto dishDto) {
+
+        this.updateById(dishDto);
+        // 清理口味数据，在增加提交的数据
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId, dishDto.getId());
+        dishFlavorService.remove(queryWrapper);
+
+        // 添加提交的数据
+        List<DishFlavor> flavors = dishDto.getFlavors();
+
+        flavors = flavors.stream().map((item) -> {
+            item.setDishId(dishDto.getId());
+            return item;
+        }).collect(Collectors.toList());
         dishFlavorService.saveBatch(flavors);
     }
 }
