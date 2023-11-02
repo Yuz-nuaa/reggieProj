b15f9ecaeb3c5cf21d22d84e33d739284d89b143
zyf <nuaazyf@163.com>
2023-11-02 22:57:26 +0800

    update地址模块

---
+++ src/main/java/com/zyfproject/reggie/service/AddressBookService.java
@@ -0,0 +1,7 @@
+package com.zyfproject.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zyfproject.reggie.entity.AddressBook;
+
+public interface AddressBookService extends IService<AddressBook> {
+}
